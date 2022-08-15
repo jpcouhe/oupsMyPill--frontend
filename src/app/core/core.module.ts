cc6b270d008613b9ccce2cbd117d2b9ffacc0e16
Jean-philippe <pro@jeanphilippecouhe.fr>
2022-08-15 13:47:19 +0200

    Text

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [HeaderComponent, LandingpageComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [HeaderComponent, LandingpageComponent],
 })
 export class CoreModule {}
